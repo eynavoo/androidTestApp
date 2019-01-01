4f4838aad90036662f8c7ce91eea94a414925081
eynav <eynavc@perfectomobile.com>
2019-01-01 14:58:37 +0200

    new files for test

---
+++ app/src/main/java/com/example/eynavc/coolapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.eynavc.coolapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+        showCountTextView.setOnClickListener(){toastMe()}
+    }
+
+    fun toastMe() {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+}
